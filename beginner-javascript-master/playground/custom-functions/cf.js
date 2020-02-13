e31db00d03ad29fc97b4529f62bc8ac8109e6fcd
Lobst3rG3off <erip.pasei@gmail.com>
2020-02-13 16:46:04 -0500

    started second part of 2nd module

--- beginner-javascript-master/playground/custom-functions/cf.js
+++ beginner-javascript-master/playground/custom-functions/cf.js
@@ -1,33 +1,47 @@
-// Function Definition
-function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
-  // this is the function body
-  console.log('Running Calculate Bill!!');
-  const total = billAmount + billAmount * taxRate + billAmount * tipRate;
-  return total;
-}
+// // Function Definition
+// function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15) {
+//   // this is the function body
+//   console.log('Running Calculate Bill!!');
+//   const total = billAmount + billAmount * taxRate + billAmount * tipRate;
+//   return total;
+// }
 
-// Function Call. Or **Run**
-const wesTotal = 500;
-const wesTaxRate = 0.3;
-// const myTotal = calculateBill(wesTotal, wesTaxRate);
+// // Function Call. Or **Run**
+// const wesTotal = 500;
+// const wesTaxRate = 0.3;
+// // const myTotal = calculateBill(wesTotal, wesTaxRate);
 
-// Function Definition
-function sayHiTo(firstName) {
-  return `Hello ${firstName}`;
-}
+// // Function Definition
+// function sayHiTo(firstName) {
+//   return `Hello ${firstName}`;
+// }
 
-// const greeting = sayHiTo('Wes');
-// console.log(greeting);
+// // const greeting = sayHiTo('Wes');
+// // console.log(greeting);
 
-function doctorize(name) {
-  return `Dr. ${name}`;
-}
+// function doctorize(name) {
+//   return `Dr. ${name}`;
+// }
 
-function yell(name = 'Silly Goose') {
-  return `HEY ${name.toUpperCase()}`;
-}
+// function yell(name = 'Silly Goose') {
+//   return `HEY ${name.toUpperCase()}`;
+// }
 
-const myBill4 = calculateBill(100, undefined, 0.2);
-console.log(myBill4);
+// const myBill4 = calculateBill(100, undefined, 0.2);
+// console.log(myBill4);
 
+//function definition
+function calculateBill() {
+    //this is the function body
+    console.log('Running calculate bill')
+const total = 100 * 1.13; 
+console.log(total)
+return total;
 
+};
+
+//function call
+const myTotal = calculateBill();
+
+console.log(`Your total is $${myTotal}`);
+console.log(`Your total is a $${calculateBill()}`);
