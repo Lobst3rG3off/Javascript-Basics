581fa1c37057f62bf6b4fcffb2c8d9263bb56cd6
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-12 18:40:08 -0400

    populating the modal

--- beginner-javascript-master/exercises/34 - Click Outside/click-outside.js
+++ beginner-javascript-master/exercises/34 - Click Outside/click-outside.js
@@ -1,7 +1,16 @@
 const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
 
-function handleCardButtonCLick() {
-  console.log('you clicked it!');
+function handleCardButtonCLick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  //  grab the image source
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description;
+  //  populate the modal with the new info
+  modalInner.innerHTML = `
+  
+  `
 }
 
 cardButtons.forEach(button =>
