6f231075c59103b7b02664dda5164ff1fb7c1f18
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-13 17:59:05 -0400

    added listener for strong tag.

--- beginner-javascript-master/exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ beginner-javascript-master/exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,7 +1,7 @@
 const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
 
 terms.addEventListener('scroll', function(e) {
-  console.log(e);
+  console.log(e.currentTarget.scrollTop);
+  console.log(e.currentTarget.scrollHeight);
 });
-
-
