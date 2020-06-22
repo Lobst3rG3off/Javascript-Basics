d6f6388ef4eab65118f657289b971ec79c0ebb0b
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-22 16:00:12 -0400

    tabs work

--- beginner-javascript-master/exercises/36 - Tabs/tabs.js
+++ beginner-javascript-master/exercises/36 - Tabs/tabs.js
@@ -7,12 +7,24 @@ const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
 function handleTabClick(e) {
   // hide all tab panels
   console.log(tabPanels);
-  tabPanels.forEach(function(panel) {
+  tabPanels.forEach(panel => {
     panel.hidden = true;
   });
   // mark all tabs as unsleected
+  tabButtons.forEach(tab => {
+    // tab.ariaSelected = false;
+    tab.setAttribute('aria-selected', false);
+  });
   // mark the clicked tab as selected
+  e.currentTarget.setAttribute('aria-selected', true);
+
   // find the associated tab panel and show it.
+  const { id } = e.currentTarget;
+
+  const tabPanel = tabs.querySelector(`[aria-labelledby="${id}"]`);
+  tabPanel.hidden = false;
+
+  //   tabPanels.forEach(element => {});
 }
 
 tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
