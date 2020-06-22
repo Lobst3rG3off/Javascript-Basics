b827c9c64b9c9007971d675005d8d937983f8257
Geoff Grossman <geoff.grossman.dev@gmail.com>
2020-06-22 15:44:05 -0400

    creating a function to handle tabclicks

--- beginner-javascript-master/exercises/36 - Tabs/tabs.js
+++ beginner-javascript-master/exercises/36 - Tabs/tabs.js
@@ -1,1 +1,11 @@
 console.log('ya ya wes we get it.. IT WORKS!');
+
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(e) {
+  console.log(e.currentTarget);
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
